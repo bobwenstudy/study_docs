f2653594694a059329305a9d81fa0c88a937e41f
wenbo13579 <wenbo13579@163.com>
2022-08-03 19:27:08 +0800

    docs_update

---
+++ docs/source/conf.py
@@ -0,0 +1,28 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'study_docs'
+copyright = '2022, CoderBob'
+author = 'CoderBob'
+release = '0.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions =  ['recommonmark','sphinx_markdown_tables'] 
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+language = 'zh_CN'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+import sphinx_rtd_theme
+html_theme = 'sphinx_rtd_theme'
+html_static_path = [sphinx_rtd_theme.get_html_theme_path()]
---
+++ docs/source/index.rst
@@ -0,0 +1,22 @@
+.. study_docs documentation master file, created by
+   sphinx-quickstart on Wed Aug  3 19:18:33 2022.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to study_docs's documentation!
+======================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+   
+   Typora+PicGo+腾讯云.md
+
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
